3021d8c82465d0f589707474c22f1c8812598a62
温格投研院 <373884306@qq.com>
2026-07-27 08:08:26 +0800

    fix: align equity chart dimensions

--- client/src/components/PnlChart.tsx
+++ client/src/components/PnlChart.tsx
@@ -780,7 +780,7 @@ export default function PnlChart() {
           }}
         >
           <ResponsiveContainer width="100%" height="100%">
-            <ComposedChart data={reviewMode ? reviewChartData : chartData} margin={{ top: 14, right: assetTrendVisible ? 78 : 62, left: 8, bottom: 10 }}>
+            <ComposedChart data={reviewMode ? reviewChartData : chartData} margin={{ top: 14, right: 78, left: 8, bottom: 10 }}>
               <defs>
                 <linearGradient id="accountPerformanceGrad" x1="0" y1="0" x2="0" y2="1">
                   <stop offset="5%" stopColor="oklch(68% 0.15 145)" stopOpacity={0.28} />
